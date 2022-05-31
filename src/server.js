8b7dab57b393db5e0411ef817bede9727974ce88
Choi HY <chlgkduddlek@gmail.com>
2022-05-31 14:19:01 +0900

    mongod setup

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import './db';
 import express from 'express';
 import morgan from 'morgan';
 import globalRouter from './routers/globalRouter';
@@ -18,5 +19,5 @@ app.use('/videos', videoRouter);
 app.use('/users', userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅Server listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
