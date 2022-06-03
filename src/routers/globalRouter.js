f3bace87f3f1de494cb25ca1d466fdc4014ebcf4
Choi HY <chlgkduddlek@gmail.com>
2022-06-03 12:15:03 +0900

    await

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { join, login } from '../controllers/userController';
-import { home, search } from '../controllers/videoController';
+import { home } from '../controllers/videoController';
 
 const globalRouter = express.Router();
 
